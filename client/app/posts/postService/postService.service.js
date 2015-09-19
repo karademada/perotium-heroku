02b23edb90755f43864e21b0a1749a448cc450a4
karademada <christophe.lazantsy@gmail.com>
2015-09-19 12:03:54 +0200

    delete

--- client/app/posts/postService/postService.service.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('perotiumApp')
-  .service('postService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
--- client/app/posts/productFilter/productFilter.filter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('perotiumApp')
-  .filter('productFilter', function () {
-    return function (input) {
-      return 'productFilter filter: ' + input;
-    };
-  });
